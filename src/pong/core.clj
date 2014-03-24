6f0f2c0eadc9520e5a7f7102f69940fc08f6743e
breggles <tilman.bregler@red-gate.com>
2014-03-24 20:55:10 +0000

    dicking with collisions

--- src/pong/core.clj
+++ src/pong/core.clj
@@ -41,40 +41,99 @@
 ; move
 
 (defn move-objs [world t-since-last-frm]
-  (loop [obj (first world)
-         rest-world (rest world) 
-         new-world []]
-    (if obj
-      (let [new-obj (if-let [move (:move obj)]
-                      (move obj world t-since-last-frm)
-                      obj)]
-        (recur 
-          (first rest-world)
-          (rest rest-world)
-          (conj new-world new-obj)))
-      new-world)))
+  (reduce #(conj %1 (if-let [move (:move %2)]
+                      (move %2 t-since-last-frm)
+                      %2))
+          []
+          world))
 
-(defn move-ball [ball world t-since-last-frm] 
-  (let [other-solid-objs (remove #(and (= % ball) (:solid? %)) 
-                                 world)
-        distance (map (partial * t-since-last-frm) (:velocity ball))
-        new-pos (vec (map + (:position ball) distance))
-        collision? (<= (y-coord new-pos) 0)
-        new-ball (if collision?
-                   (let [vel (:velocity ball)] 
-                     (assoc-in ball [:velocity 1] (* (y-coord vel) -1)))
-                   ball)]
-    (assoc new-ball :position new-pos)))
+(defn move-ball [ball t-since-last-frm] 
+  (let [distance (map (partial * t-since-last-frm) (:velocity ball))
+        new-pos (vec (map + (:position ball) distance))]
+    (assoc ball :position new-pos)))
+
+(defn bottom-right [obj]
+  (map #(dec (+ %1 %2)) (:position obj) (:size obj)))
+
+(defn bounding-box [obj]
+  (vec (map int 
+            (flatten [(:position obj) (bottom-right obj)]))))
+
+(defn horz-coll? [bnd-box1 bnd-box2]
+  (when (and (or (= (get bnd-box1 1) (get bnd-box2 3))
+                 (= (get bnd-box1 3) (get bnd-box2 1)))
+             (or (<= (get bnd-box2 0) (get bnd-box1 0) (get bnd-box2 2))
+                 (<= (get bnd-box2 0) (get bnd-box1 2) (get bnd-box2 2))))
+    :horz-coll))
+
+(defn vert-coll? [bnd-box1 bnd-box2]
+  (when (and (or (= (get bnd-box1 0) (get bnd-box2 2))
+                 (= (get bnd-box1 2) (get bnd-box2 0)))
+             (or (<= (get bnd-box2 1) (get bnd-box1 1) (get bnd-box2 3))
+                 (<= (get bnd-box2 1) (get bnd-box1 3) (get bnd-box2 3))) 
+             )
+    :vert-coll))
+
+(defn detect-obj-colls [idx-obj idx-others]
+  (let [obj-bound (bounding-box (:obj idx-obj))] 
+    (for [idx-other idx-others
+          collision? [horz-coll? vert-coll?]
+          :let [other-bound (bounding-box (:obj idx-other))
+                collision (collision? obj-bound other-bound)]
+          :when collision] 
+      {:idx-objs [idx-obj idx-other] :type collision})))
+
+(defn index-solid [idx obj]
+  (when (:solid? obj) 
+    {:idx idx :obj obj}))
+
+(defn detect-collisions [world t-since-prev-frm]
+  (let [provisional-world (move-objs world t-since-prev-frm)
+        indexed-solids (keep-indexed index-solid provisional-world)]
+    (loop [idx-obj (first indexed-solids)
+           rest-idx-solids (rest indexed-solids)
+           collisions []] 
+      (if idx-obj
+        (recur 
+          (first rest-idx-solids)
+          (rest rest-idx-solids)
+          (concat collisions 
+                  (detect-obj-colls idx-obj rest-idx-solids)))
+        collisions))))
+
+(defn collide-obj [idx-obj idx-other world]
+  (let [idx (:idx idx-obj)
+        obj (nth world idx)
+        other (nth world (:idx idx-other))] 
+    (assoc world
+           idx 
+           (if-let [collide (:collide obj)]
+             (collide obj other)
+             obj))))  
+
+(defn apply-collision [world collision]
+  (let [idx-objs (:idx-objs collision)
+        frst (first idx-objs)
+        scnd (second idx-objs)]
+    (->> world
+         (collide-obj frst scnd)
+         (collide-obj scnd frst))))
+
+(defn apply-collisions [world collisions]
+  (reduce apply-collision world collisions))
 
 ; run
 
 (let [running (atom true)
       world-atom (atom [{:id :ball
                          :position [100 100]
-                         :velocity [0.1 -0.1] ; pixels/millisecond
+                         :size [10 10]
+                         :velocity [0.1 -0.1] ; pixels/millisecond 
                          :solid? true
                          :paint paint-ball
-                         :move move-ball}
+                         :move move-ball
+                         :collide (fn [a b] (alert "asdf"))
+                         }
                         {:id :top-wall
                          :position [0 -1]
                          :size [1 500]
@@ -100,7 +159,10 @@
           t-since-last-frm (t/in-millis
                              (t/interval @last-frm-time-atom frm-time))
           world @world-atom
-          new-world (move-objs world t-since-last-frm)]
+          collisions (detect-collisions world t-since-last-frm)
+          new-world (-> world 
+                        (apply-collisions collisions)
+                        (move-objs t-since-last-frm))]
       (reset! world-atom new-world)
       (reset! last-frm-time-atom frm-time))
     (repaint! canv)
