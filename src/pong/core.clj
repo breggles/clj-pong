b966be4a8d1b58f31ceb6d82f44f158cf7b7b72c
breggles <tilman.bregler@red-gate.com>
2014-03-30 18:53:01 +0100

    fix collision detection bug, make paddles stop when they collide with walls

--- src/pong/core.clj
+++ src/pong/core.clj
@@ -20,7 +20,7 @@
 
 (defn get-by-id-indexed [world id]
   (first 
-    (keep-indexed #(when (= :right-paddle (:id %2)) [%1 %2]) world)))
+    (keep-indexed #(when (= id (:id %2)) [%1 %2]) world)))
 
 ; paint
 
@@ -29,12 +29,18 @@
     (when-let [paint (:paint obj)]
       (paint canv g obj))))
 
-(defn paint-rect [canv g obj]
-  (let [pos (:position obj)
-        size (:size obj)] 
-    (draw g 
-      (rect (x-coord pos) (y-coord pos) (x-coord size) (y-coord size))
-      (style :foreground :black))))
+(defn paint-rect 
+  ([canv g obj]
+   (paint-rect canv g obj :black))
+  ([canv g obj colour]
+    (let [pos (:position obj)
+          size (:size obj)] 
+      (draw g 
+        (rect (x-coord pos) (y-coord pos) (x-coord size) (y-coord size))
+        (style :foreground colour)))))
+
+(defn paint-wall [canv g obj]
+  (paint-rect canv g obj :red))
 
 (defn paint-fps-indicator [canv g last-frm-time-atom]
   (let [now (t/now)
@@ -58,7 +64,7 @@
     (assoc ball :position new-pos)))
 
 (defn bottom-right [obj]
-  (map #(dec (+ %1 %2)) (:position obj) (:size obj)))
+  (map + (:position obj) (:size obj)))
 
 (defn bounding-box [obj]
   (vec (map #(Math/round (double %)) 
@@ -68,28 +74,26 @@
   (when (and (or (= (get bnd-box1 1) (get bnd-box2 3))
                  (= (get bnd-box1 3) (get bnd-box2 1)))
              (or (<= (get bnd-box2 0) (get bnd-box1 0) (get bnd-box2 2))
-                 (<= (get bnd-box2 0) (get bnd-box1 2) (get bnd-box2 2))))
+                 (<= (get bnd-box2 0) (get bnd-box1 2) (get bnd-box2 2))
+                 (<= (get bnd-box1 0) (get bnd-box2 0) (get bnd-box1 2))))
     :horz-coll))
 
 (defn vert-coll? [bnd-box1 bnd-box2]
   (when (and (or (= (get bnd-box1 0) (get bnd-box2 2))
                  (= (get bnd-box1 2) (get bnd-box2 0)))
              (or (<= (get bnd-box2 1) (get bnd-box1 1) (get bnd-box2 3))
-                 (<= (get bnd-box2 1) (get bnd-box1 3) (get bnd-box2 3))) 
-             )
+                 (<= (get bnd-box2 1) (get bnd-box1 3) (get bnd-box2 3))
+                 (<= (get bnd-box1 1) (get bnd-box2 1) (get bnd-box1 3))))
     :vert-coll))
 
 (defn detect-obj-colls [idx-obj idx-others]
   (let [obj-bound (bounding-box (:obj idx-obj))] 
-    ;(println obj-bound)
     (for [idx-other idx-others
           collision? [horz-coll? vert-coll?]
           :let [other-bound (bounding-box (:obj idx-other))
                 collision (collision? obj-bound other-bound)]
           :when collision] 
-      (do
-        ;(println collision)
-        {:idx-objs [idx-obj idx-other] :type collision}))))
+      {:idx-objs [idx-obj idx-other] :type collision})))
 
 (defn index-solid [idx obj]
   (when (:solid? obj) 
@@ -109,9 +113,14 @@
                   (detect-obj-colls idx-obj rest-idx-solids)))
         collisions))))
 
-(defn collide-ball [obj other coll-type] 
-  (let [vel (:velocity obj)] 
-    (assoc obj :velocity (case coll-type
+(defn collide-paddle [paddle other coll-type]
+  (if (some #{(:id other)} [:top-wall :bottom-wall])
+    (assoc-in paddle [:velocity 1] 0)
+    paddle))
+
+(defn collide-ball [ball other coll-type] 
+  (let [vel (:velocity ball)] 
+    (assoc ball :velocity (case coll-type
                            :horz-coll [(x-coord vel) 
                                        (* -1 (y-coord vel))]
                            :vert-coll [(* -1 (x-coord vel))
@@ -147,17 +156,28 @@
         (apply-collisions collisions)
         (move-objs t-since-last-frm))))
 
+(defn update-paddle-vel [world id vert-velocity]
+  (let [idx-paddle (get-by-id-indexed world id)
+        idx (first idx-paddle)]
+    (assoc-in world [idx :velocity 1] vert-velocity)))
+
 (defn key-pressed [world keyChar] 
-  (let [idx-rtpdl (get-by-id-indexed world :right-paddle)
-        idx (first idx-rtpdl)]
-    (assoc-in world [idx :velocity 1] (case keyChar \k 0.1 \i -0.1 0))))
+  (case keyChar 
+    \k (update-paddle-vel world :right-paddle  0.1) 
+    \i (update-paddle-vel world :right-paddle -0.1) 
+    \s (update-paddle-vel world :left-paddle   0.1)  
+    \w (update-paddle-vel world :left-paddle  -0.1)
+    world))
 
 (defn key-released [world keyChar] 
-  (let [idx-rtpdl (get-by-id-indexed world :right-paddle)
-        idx (first idx-rtpdl)]
-    (assoc-in world [idx :velocity 1] 0)))
+  (case keyChar 
+    \k (update-paddle-vel world :right-paddle 0) 
+    \i (update-paddle-vel world :right-paddle 0) 
+    \s (update-paddle-vel world :left-paddle  0)  
+    \w (update-paddle-vel world :left-paddle  0)
+    world))
 
-(defn key-listen [e world-atom handler]
+(defn key-listen! [e world-atom handler]
   (let [keyChar (.getKeyChar e)] 
     (swap! world-atom handler keyChar)))
 
@@ -172,54 +192,72 @@
                :title "Pong"
                :width 500
                :height 500 
-               :resizable? false
+               ;:resizable? false
                :content canv
                :on-close :dispose)]
-  (listen frm :window-closing (fn [_] (reset! run?-atom false)))
-  (listen canv :key-pressed (fn [e] (key-listen e world-atom key-pressed)))
-  (listen canv :key-released (fn [e] (key-listen e world-atom key-released)))
-  (.requestFocusInWindow canv)
-  (show! frm)
-  canv))
+    (listen frm :window-closing (fn [_] (reset! run?-atom false)))
+    (listen canv :key-pressed (fn [e] (key-listen! e world-atom key-pressed)))
+    (listen canv :key-released (fn [e] (key-listen! e world-atom key-released)))
+    (.requestFocusInWindow canv)
+    (show! frm)
+    ;(.setExtendedState frm javax.swing.JFrame/MAXIMIZED_BOTH)
+    ; wait for maximization
+    (Thread/sleep 200)
+    canv))
+
+(defn init-world [world-atom canv]
+  (let [canv-width (width canv)
+        canv-height (height canv)]
+    [{:id :ball
+      :position [100 100]
+      :size [10 10]
+      :velocity [0.1 -0.1] ; pixels/millisecond 
+      :solid? true
+      :paint paint-rect
+      :move move-obj
+      :collide collide-ball}
+     {:id :top-wall
+      :position [0 0]
+      :size [canv-width 0]
+      :paint paint-wall
+      :solid? true}
+     {:id :right-wall
+      :position [canv-width 0]
+      :size [0 canv-height]
+      :paint paint-wall
+      :solid? true}
+     {:id :bottom-wall
+      :position [0 (- canv-height 2)]
+      :size [canv-width 0]
+      :paint paint-wall
+      :solid? true}
+     {:id :left-wall
+      :position [0 0]
+      :size [0 canv-height]
+      :paint paint-wall
+      :solid? true}
+     {:id :right-paddle
+      :position [(- canv-width 50) (- (/ canv-height 2) 35)]
+      :size [10 70]
+      :velocity [0 0]
+      :solid? true
+      :paint paint-rect
+      :move move-obj
+      :collide collide-paddle}  
+     {:id :left-paddle
+      :position [50 (- (/ canv-height 2) 35)]
+      :size [10 70]
+      :velocity [0 0]
+      :solid? true
+      :paint paint-rect
+      :move move-obj
+      :collide collide-paddle}]))
 
 (let [run?-atom (atom true)
       last-frm-time-atom (atom (t/now))
-      world-atom (atom [{:id :ball
-                         :position [100 100]
-                         :size [10 10]
-                         :velocity [0.1 -0.1] ; pixels/millisecond 
-                         :solid? true
-                         :paint paint-rect
-                         :move move-obj
-                         :collide collide-ball}
-                        {:id :top-wall
-                         :position [50 50]
-                         :size [400 0]
-                         :paint paint-rect
-                         :solid? true}
-                        {:id :right-wall
-                         :position [450 50]
-                         :size [0 400]
-                         :paint paint-rect
-                         :solid? true}
-                        {:id :bottom-wall
-                         :position [50 450]
-                         :size [400 0]
-                         :paint paint-rect
-                         :solid? true}
-                        {:id :left-wall
-                         :position [50 50]
-                         :size [0 400]
-                         :paint paint-rect
-                         :solid? true}
-                        {:id :right-paddle
-                         :position [350 200]
-                         :size [10 70]
-                         :velocity [0 0]
-                         :solid? true
-                         :paint paint-rect
-                         :move move-obj}])
+      world-atom (atom nil)
       canv (init-canvas world-atom run?-atom)]
+  (swap! world-atom init-world canv)
   (reset! last-frm-time-atom (t/now)) 
   (while @run?-atom
     (let [frm-time (t/now)
--- test/pong/core_test.clj
+++ test/pong/core_test.clj
@@ -3,26 +3,26 @@
             [pong.core :refer :all]))
 
 (deftest tests
-  (testing "moving ball returns new ball with correct position"
+  (testing "moving obj returns new obj with correct position"
     (is (= {:position [1 0]
             :velocity [1 0]} 
-           (move-ball {:position [0 0]
+           (move-obj {:position [0 0]
                        :velocity [1 0]}
                       1))))
   (testing "moving objects returns new world with objects at new positions"
     (is (= [{:position [1 0]
              :velocity [1 0]
-             :move move-ball
-             :paint paint-ball}]
+             :move move-obj
+             :paint paint-rect}]
            (move-objs [{:position [0 0]
                         :velocity [1 0]
-                        :move move-ball
-                        :paint paint-ball}]
+                        :move move-obj
+                        :paint paint-rect}]
                       1))))
   (testing "can calculate bottom right corner correctly"
-    (is (= [8 9] (bottom-right {:position [1 3] :size [8 7]}))))
+    (is (= [9 10] (bottom-right {:position [1 3] :size [8 7]}))))
   (testing "can calc bounding box"
-    (is (= [1 3 8 9] (bounding-box {:position [1.1 3.4] :size [8 7]}))))
+    (is (= [1 3 9 10] (bounding-box {:position [1.1 3.4] :size [8 7]}))))
   (testing "solids are indexed correctly"
     (do (is (= {:idx 3 :obj {:solid? true}} (index-solid 3 {:solid? true})))
         (is (= nil (index-solid 3 {:solid? false})))))
@@ -38,14 +38,14 @@
     (is (= [{:idx-objs [{:idx 0 
                          :obj {:position [0 0] :size [5 5]}}
                         {:idx 1 
-                         :obj {:position [4 0] :size [5 5]}}]
+                         :obj {:position [5 0] :size [5 5]}}]
             :type :vert-coll}]
            (detect-obj-colls {:idx 0 
                               :obj {:position [0 0] :size [5 5]}}
                              [{:idx 1
-                               :obj {:position [4 0] :size [5 5]}}
+                               :obj {:position [5 0] :size [5 5]}}
                               {:idx 2
-                               :obj {:position [5 0] :size [5 5]}}]))))
+                               :obj {:position [6 0] :size [5 5]}}]))))
   (testing "collisions in world are detected"
     (is (= [{:idx-objs [{:idx 0 
                          :obj {:position [0 0] 
@@ -53,7 +53,7 @@
                                :velocity [0 0]
                                :solid? true}}
                         {:idx 1 
-                         :obj {:position [4 0] 
+                         :obj {:position [5 0] 
                                :size [5 5]
                                :velocity [0 0]
                                :solid? true}}]
@@ -62,11 +62,11 @@
                                       :size [5 5]
                                       :velocity [0 0]
                                       :solid? true}
-                               {:position [4 0] 
+                               {:position [5 0] 
                                       :size [5 5]
                                       :velocity [0 0]
                                       :solid? true}
-                               {:position [5 0] 
+                               {:position [6 0] 
                                       :size [5 5]
                                       :velocity [0 0]
                                       :solid? true}]
@@ -99,4 +99,51 @@
                                :solid? true}]))))
   (testing "apply-collision calls collide-obj twice"
     (is (= 2 (with-redefs [collide-obj (fn [_ _ _ w] (inc w))] 
-               (apply-collision 0 {}))))))
+               (apply-collision 0 {})))))
+  (testing "bounding box bottom wall"
+    (is (= [0 500 500 500] 
+           (bounding-box {:id :bottom-wall
+                          :position [0 500]
+                          :size [500 0]
+                          :solid? true}))))
+  (testing "bounding box paddle"
+    (is (= [450 430 460 500] 
+           (bounding-box {:id :right-paddle
+                          :position [450 430]
+                          :size [10 70]
+                          :velocity [0 0]
+                          :solid? true}))))
+  (testing "horzizontal collide"
+    (is (= :horz-coll (horz-coll? [0 500 500 500] [450 430 460 500]))))
+  (testing "paddle can collide horizontally"
+    (is (= :horz-coll (horz-coll? 
+                        (bounding-box {:id :bottom-wall
+                                       :position [0 500]
+                                       :size [500 0]
+                                       :solid? true})
+                        (bounding-box {:id :right-paddle
+                                       :position [450 430]
+                                       :size [10 70]
+                                       :velocity [0 0]
+                                       :solid? true})))))
+  (testing "paddle can collide with wall"
+    (is (= :horz-coll 
+           (:type 
+             (first
+               (detect-obj-colls {:idx 3 
+                                  :obj {:id :bottom-wall
+                                        :position [0 500]
+                                        :size [500 0]
+                                        :solid? true}}
+                                 [{:idx 5
+                                   :obj {:id :right-paddle
+                                         :position [450 430]
+                                         :size [10 70]
+                                         :velocity [0 0]
+                                         :solid? true}}]))))))
+  (testing "if paddle collides with top or bottom wall vert velocity is set to 0"
+    (is (= 0 (get-in (collide-paddle {:velocity [1 2]}
+                                     {:id :top-wall}
+                                     :horz-coll)
+                     [:velocity 1])))))
+
